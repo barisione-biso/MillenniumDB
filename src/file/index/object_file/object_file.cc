05df88bf571aa0cf042afe0f7fa6c6253c7b75e3
Carlos Rojas <cirojas@zoho.com>
2019-12-12 00:10:43 -0300

    added comentaries to ObjectFile

--- src/file/index/object_file/object_file.cc
+++ src/file/index/object_file/object_file.cc
@@ -9,39 +9,46 @@ ObjectFile::ObjectFile(const string& filename)
     : file (FileManager::get_file(FileManager::get_file_id(filename)))
 {
     file.seekg (0, file.end);
-    if (file.tellg() == 0) { // Write trash to prevent the id = 0
+    // If the file is empty, write a trash byte to prevent the ID = 0
+    if (file.tellg() == 0) {
         char c = 0;
         file.write(&c, 1);
     }
 }
 
-ObjectFile::~ObjectFile()
-{
+
+ObjectFile::~ObjectFile()  {
     file.close();
 }
 
-unique_ptr<vector<char>> ObjectFile::read(uint64_t id)
-{
+
+unique_ptr<vector<char>> ObjectFile::read(uint64_t id) {
+    // set position=`id` and read the length of the object
     file.seekg(id);
-
-    char length_b[4];
+    char length_b[4]; // array of 4 bytes to store the length as bytes
     file.read(length_b, 4);
+    uint32_t length = *(uint32_t *)length_b;
 
-    int length = *(int *)length_b;
-    // TODO: check if length is reasonable to check for possible errors?
+    // read the following `length` bytes
     auto value = make_unique<vector<char>>(length);
     file.read(&(*value)[0], length);
-
     return value;
 }
 
-uint64_t ObjectFile::write(vector<char>& bytes){
-    int length = bytes.size();
 
+uint64_t ObjectFile::write(vector<char>& bytes) {
+    // set position at end of file
     file.seekg(0, file.end);
-    int write_pos = file.tellg();
 
+    // remember the actual position where we are writing the object
+    uint64_t write_pos = file.tellg();
+
+    // write to the file the length in bytes
+    uint32_t length = bytes.size();
     file.write((const char *)&length, 4);
+
+    // write to the file the bytes of the object
     file.write(bytes.data(), length);
+
     return write_pos;
 }
