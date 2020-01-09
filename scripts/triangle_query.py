bb7328d68c1f0d92a4b47fb0ed7de6730f3a43ee
Carlos Rojas <cirojas@zoho.com>
2020-01-09 13:49:54 -0300

    triangle query for rdf

--- scripts/triangle_query.py
+++ scripts/triangle_query.py
@@ -33,3 +33,48 @@ with open("edges.txt", mode="w") as edges_file:
     for n in range(1, N):
         edges_file.write("({})->({}) :T".format(n+1, N))
         edges_file.write("\n")
+
+# ?x <http://example.org/label> "Person" .
+# ?y <http://example.org/label> "Person" .
+# ?z <http://example.org/label> "Person" .
+# ?e1 <http://example.org/label> "R" .
+# ?e2 <http://example.org/label> "S" .
+# ?e3 <http://example.org/label> "T" .
+# ?x ?e1 ?y .
+# ?y ?e2 ?z .
+# ?x ?e3 ?z .
+
+
+with open("triangle.nt", mode="w") as rfd_file:
+    for n in range(N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/label> \"Person\" .\n".format(n+1))
+    edges_count = 1
+    # R = (1,1), (1,2),..., (1,N) ; (1,N), (2,N), ... (N,N)
+    for n in range(N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}> .\n".format(1, edges_count, n+1))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"R\" .\n".format(edges_count))
+        edges_count += 1
+    for n in range(1, N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}> .\n".format(n+1, edges_count, N))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"R\" .\n".format(edges_count))
+        edges_count += 1
+
+    # S = (1,1), (2,1),..., (N,1) ; (N,1), (N,2), ... (N,N)
+    for n in range(N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}> .\n".format(n+1, edges_count, 1))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"S\" .\n".format(edges_count))
+        edges_count += 1
+    for n in range(1, N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}> .\n".format(N, edges_count, n+1))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"S\" .\n".format(edges_count))
+        edges_count += 1
+
+    # T = (1,1), (1,2),..., (1,N) ; (1,N), (2,N), ... (N,N)
+    for n in range(N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}> .\n".format(1, edges_count, n+1))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"T\" .\n".format(edges_count))
+        edges_count += 1
+    for n in range(1, N):
+        rfd_file.write("<http://example.org/nodes/{}> <http://example.org/edges/{}> <http://example.org/nodes/{}>. \n".format(n+1, edges_count, N))
+        rfd_file.write("<http://example.org/edges/{}> <http://example.org/label> \"T\" .\n".format(edges_count))
+        edges_count += 1
