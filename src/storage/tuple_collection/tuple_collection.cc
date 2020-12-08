d8b160c188b1c90cc2f1dc722edcfcb47085e065
Juan Romero <jaromero6@uc.cl>
2020-12-08 12:57:46 -0300

    fix sizeof of uint64

--- src/storage/tuple_collection/tuple_collection.cc
+++ src/storage/tuple_collection/tuple_collection.cc
@@ -3,12 +3,14 @@
 #include <cassert>
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 #include <cmath>
 
 #include "storage/file_id.h"
 #include "storage/file_manager.h"
 #include "storage/buffer_manager.h"
 
+using namespace std;
 
 TupleCollection::TupleCollection(Page& page, const size_t tuple_size) :
     page(page),
@@ -20,13 +22,15 @@ TupleCollection::TupleCollection(Page& page, const size_t tuple_size) :
 
 
 TupleCollection::~TupleCollection() {
+  // TODO: VER FLUSH
   page.make_dirty();
 	buffer_manager.unpin(page);
+  file_manager.flush(page.page_id, page.get_bytes());
 }
 
 
 bool TupleCollection::is_full() const {
-    if (((*tuple_count) + 1) * tuple_size + sizeof(tuple_count) < Page::PAGE_SIZE) {
+    if (((*tuple_count) + 1) * tuple_size * sizeof(uint64_t) + sizeof(tuple_count) < Page::PAGE_SIZE) {
       return false;
     }
     return true;
@@ -39,13 +43,9 @@ uint64_t TupleCollection::get_n_tuples() const {
 
 void TupleCollection::add(std::vector<uint64_t> new_tuple) {
 	  for (size_t i = 0; i < tuple_size; i++) {
-      if (sizeof(new_tuple[i]) != sizeof(uint64_t)) {
-        std::cout <<"eeror\n";
-      }
     	tuples[(*tuple_count) * tuple_size + i] = new_tuple[i];
   	}
   	(*tuple_count)++;
-    page.make_dirty();
 }
 
 
@@ -65,6 +65,11 @@ void TupleCollection::override_tuple(std::vector<uint64_t>& tuple, int position)
 }
 
 
+void TupleCollection::reset() {
+    (*tuple_count) = 0;
+}
+
+
 void TupleCollection::swap(int x, int y) {
     std::vector<uint64_t> x_tuple = get(x);
     std::vector<uint64_t> y_tuple = get(y);
@@ -75,7 +80,6 @@ void TupleCollection::swap(int x, int y) {
 
 void TupleCollection::sort( bool (*has_priority)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars),std::vector<uint64_t> order_vars) {
     quicksort(0, (*tuple_count) - 1, has_priority, order_vars);
-    page.make_dirty();
 }
 
 
@@ -103,4 +107,93 @@ int TupleCollection::partition(int i, int f, bool (*has_priority)(std::vector<ui
     }
     TupleCollection::swap(j, f);
     return j;
+}
+
+MergeOrderedTupleCollection::MergeOrderedTupleCollection(
+    size_t tuple_size,
+    std::vector<uint_fast64_t> order_vars,
+    bool (*has_priority)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars)) :
+        tuple_size(tuple_size),
+        order_vars(order_vars),
+        has_priority(has_priority),
+        left_tuple(std::vector<uint_fast64_t>(tuple_size)),
+        right_tuple(std::vector<uint_fast64_t>(tuple_size)) { }
+
+
+void MergeOrderedTupleCollection::merge(
+    uint_fast64_t left_start,
+    uint_fast64_t left_end,
+    uint_fast64_t right_start,
+    uint_fast64_t right_end,
+    FileId source_file_id,
+    FileId output_file_id
+    ) {
+    auto left_run = std::make_unique<TupleCollection>(buffer_manager.get_page(source_file_id, left_start), tuple_size);
+    auto right_run = std::make_unique<TupleCollection>(buffer_manager.get_page(source_file_id, right_start), tuple_size);
+    auto out_run = std::make_unique<TupleCollection>(buffer_manager.get_page(output_file_id, left_start), tuple_size);
+    left_tuple = left_run->get(0);
+    right_tuple = right_run->get(0);
+    uint_fast64_t left_counter = 0;
+    uint_fast64_t right_counter = 0;
+    uint_fast64_t out_page_counter = left_start;
+    bool open_left = true;
+    bool open_right = true;
+    while (open_left && open_right) {
+        if (out_run->is_full()) {
+            out_page_counter++;
+            out_run = std::make_unique<TupleCollection>(buffer_manager.get_page(output_file_id, out_page_counter), tuple_size);
+            out_run->reset();
+        }
+        if ((has_priority(left_tuple, right_tuple, order_vars) || (right_start > right_end)) && open_left) {
+            out_run->add(left_tuple);
+            left_counter++;
+            if (left_counter == left_run->get_n_tuples()) {
+                left_start++;
+                if (left_start <= left_end) {
+                    left_run = std::make_unique<TupleCollection>(buffer_manager.get_page(source_file_id, left_start), tuple_size);
+                    left_counter = 0;
+                } else {
+                    open_left = false;
+                    continue;
+                }
+            }
+            left_tuple = left_run->get(left_counter);
+        }
+        else if ((has_priority(right_tuple, left_tuple, order_vars) || (left_start > left_end)) && open_right) {
+            out_run->add(right_tuple);
+            right_counter++;
+            if (right_counter == right_run->get_n_tuples()) {
+                right_start++;
+                if (right_start <= right_end) {
+                    right_run = std::make_unique<TupleCollection>(buffer_manager.get_page(output_file_id, right_start), tuple_size);
+                    right_counter = 0;
+                } else {
+                    open_right = false;
+                    continue;
+                }
+            }
+            right_tuple = right_run->get(right_counter);
+        }
+    }
+}
+
+
+void MergeOrderedTupleCollection::copy_page(
+      uint_fast64_t source_page,
+      FileId source_file_id,
+      FileId output_file_id
+    ) {
+      auto source_tuples = make_unique<TupleCollection>(buffer_manager.get_page(source_file_id, source_page), tuple_size);
+      auto output_tuples = make_unique<TupleCollection>(buffer_manager.get_page(output_file_id, source_page), tuple_size);
+      output_tuples->reset();
+      for (size_t i = 0; i < source_tuples->get_n_tuples(); i++) {
+          std::vector<uint64_t> t = source_tuples->get(i);
+          uint64_t a = t[0];
+          output_tuples->add(move(t));
+          uint64_t b = output_tuples->get(i)[0];
+          if (a != b) {
+            cout << "algo malo hay\n";
+          }
+      }
+      //source_tuples->reset();
 }
--- src/storage/tuple_collection/tuple_collection.h
+++ src/storage/tuple_collection/tuple_collection.h
@@ -7,6 +7,7 @@
 
 #include "storage/page.h"
 #include "storage/page_id.h"
+#include "storage/file_id.h"
 
 class MergeOrderedTupleCollection;
 
@@ -20,13 +21,13 @@ public:
     uint64_t get_n_tuples() const;
     void add(std::vector<uint64_t> new_tuple);
     void sort(bool (*is_leq)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars),std::vector<uint64_t> order_vars);
-    const size_t tuple_size;
+    void reset();
 
 private:
     Page& page;
+    const size_t tuple_size;
     uint64_t* tuple_count;
     uint64_t* tuples;
-
     void swap(int x, int y);
     void override_tuple(std::vector<uint64_t>& bytes, int position);
     int partition(int i, int f, bool (*is_leq)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars), std::vector<uint64_t> order_vars);
@@ -35,6 +36,30 @@ private:
 };
 
 class MergeOrderedTupleCollection {
+public:
+    MergeOrderedTupleCollection(
+        size_t tuple_size,
+        std::vector<uint64_t> order_vars,
+        bool (*is_leq)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars));
+    void merge(
+        uint_fast64_t left_start,
+        uint_fast64_t left_end,
+        uint_fast64_t right_start,
+        uint_fast64_t right_end,
+        FileId source_file_id,
+        FileId output_file_id
+        );
+    void copy_page(
+      uint_fast64_t source_page,
+      FileId source_file_id,
+      FileId output_file_id
+    );
+private:
+    size_t tuple_size;
+    std::vector<uint_fast64_t> order_vars;
+    bool (*has_priority)(std::vector<uint64_t> x, std::vector<uint64_t> y, std::vector<uint64_t> order_vars);
+    std::vector<uint_fast64_t> left_tuple;
+    std::vector<uint_fast64_t> right_tuple;
 };
 
 #endif  // STORAGE__TUPLE_COLLECTION_H_
