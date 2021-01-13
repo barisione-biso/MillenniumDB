4eab95113bf9371bf5dcbaf7bbc6659fc65f3a4f
Carlos Rojas <cirojas6@uc.cl>
2021-01-12 22:14:17 -0300

    fixing wrong assumption in EdgeTableLookup, now checking if ObjectId corresponds to an Edge

--- src/relational_model/execution/binding_id_iter/edge_table_lookup.cc
+++ src/relational_model/execution/binding_id_iter/edge_table_lookup.cc
@@ -41,63 +41,66 @@ bool EdgeTableLookup::next() {
         ++lookups;
 
         auto edge_assignation = (*my_input)[edge];
-        auto edge_id = QuadModel::VALUE_MASK & edge_assignation.id;
-
-        assert(edge_id > 0);
-        auto record = table[edge_id - 1]; // first edge has the id 1, and its inserted at pos 0 in the table
-        if (record == nullptr) {
+        if ( (QuadModel::VALUE_MASK & edge_assignation.id) != QuadModel::CONNECTION_MASK) {
             return false;
-        } else {
-            my_binding.add_all(*my_input);
-
-            // check if assignated variables (not null) have the same value
-            if (std::holds_alternative<VarId>(from)) {
-                auto from_value = (*my_input)[std::get<VarId>(from)];
-                if (!from_value.is_null() && from_value.id != record->ids[0]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(from), ObjectId(record->ids[0]));
-                }
-            } else { // std::holds_alternative<ObjectId>(from)
-                auto from_value = std::get<ObjectId>(from);
-                if (!from_value.is_null() && from_value.id != record->ids[0]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(from), ObjectId(record->ids[0]));
-                }
-            }
-            if (std::holds_alternative<VarId>(to)) {
-                auto to_value = (*my_input)[std::get<VarId>(to)];
-                if (!to_value.is_null() && to_value.id != record->ids[1]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(to), ObjectId(record->ids[1]));
-                }
-            } else { // std::holds_alternative<ObjectId>(to)
-                auto to_value = std::get<ObjectId>(to);
-                if (!to_value.is_null() && to_value.id != record->ids[1]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(to), ObjectId(record->ids[1]));
-                }
-            }
-            if (std::holds_alternative<VarId>(type)) {
-                auto type_value = (*my_input)[std::get<VarId>(type)];
-                if (!type_value.is_null() && type_value.id != record->ids[2]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(type), ObjectId(record->ids[2]));
-                }
-            } else { // std::holds_alternative<ObjectId>(type)
-                auto type_value = std::get<ObjectId>(type);
-                if (!type_value.is_null() && type_value.id != record->ids[2]) {
-                    return false;
-                } else {
-                    my_binding.add(std::get<VarId>(type), ObjectId(record->ids[2]));
-                }
-            }
-            ++results;
-            return true;
         }
+        auto edge_id = QuadModel::VALUE_MASK & edge_assignation.id;
+        assert(edge_id > 0);
+
+        auto record = table[edge_id - 1]; // first edge has the id 1, and its inserted at pos 0 in the table
+
+        // if mask was and edge this should not be nullptr
+        assert(record != nullptr);
+
+        my_binding.add_all(*my_input);
+
+        // check if assignated variables (not null) have the same value
+        if (std::holds_alternative<VarId>(from)) {
+            auto from_value = (*my_input)[std::get<VarId>(from)];
+            if (!from_value.is_null() && from_value.id != record->ids[0]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(from), ObjectId(record->ids[0]));
+            }
+        } else { // std::holds_alternative<ObjectId>(from)
+            auto from_value = std::get<ObjectId>(from);
+            if (!from_value.is_null() && from_value.id != record->ids[0]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(from), ObjectId(record->ids[0]));
+            }
+        }
+        if (std::holds_alternative<VarId>(to)) {
+            auto to_value = (*my_input)[std::get<VarId>(to)];
+            if (!to_value.is_null() && to_value.id != record->ids[1]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(to), ObjectId(record->ids[1]));
+            }
+        } else { // std::holds_alternative<ObjectId>(to)
+            auto to_value = std::get<ObjectId>(to);
+            if (!to_value.is_null() && to_value.id != record->ids[1]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(to), ObjectId(record->ids[1]));
+            }
+        }
+        if (std::holds_alternative<VarId>(type)) {
+            auto type_value = (*my_input)[std::get<VarId>(type)];
+            if (!type_value.is_null() && type_value.id != record->ids[2]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(type), ObjectId(record->ids[2]));
+            }
+        } else { // std::holds_alternative<ObjectId>(type)
+            auto type_value = std::get<ObjectId>(type);
+            if (!type_value.is_null() && type_value.id != record->ids[2]) {
+                return false;
+            } else {
+                my_binding.add(std::get<VarId>(type), ObjectId(record->ids[2]));
+            }
+        }
+        ++results;
+        return true;
     }
 }
