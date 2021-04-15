aede22c3577ea1f96b2a66aae1887223680c2cf8
Juan Romero <jaromero6@uc.cl>
2021-04-15 17:36:00 -0400

    Add property path enum

---
+++ src/relational_model/execution/binding_id_iter/property_path_enum.h
@@ -0,0 +1,62 @@
+#ifndef RELATIONAL_MODEL__PROPERTY_PATH_ENUM_H_
+#define RELATIONAL_MODEL__PROPERTY_PATH_ENUM_H_
+
+#include <array>
+#include <memory>
+#include <set>
+#include <queue>
+#include <variant>
+
+#include "base/binding/binding_id_iter.h"
+#include "base/parser/logical_plan/op/path_automaton/path_automaton.h"
+#include "base/property_paths/search_state.h"
+#include "relational_model/execution/binding_id_iter/scan_ranges/scan_range.h"
+#include "storage/index/bplus_tree/bplus_tree.h"
+
+// TODO: Comment resume
+
+class PropertyPathEnum : public BindingIdIter {
+    using Id = std::variant<VarId, ObjectId>;
+
+private:
+    // Attributes determined in the constuctor
+
+    BPlusTree<4>& type_from_to_edge;  // Used to search foward
+    BPlusTree<4>& to_type_from_edge;  // Used to search backward
+
+    Id start;
+    VarId end;
+    PathAutomaton automaton;
+
+
+    // Attributes determined in begin
+    BindingId* parent_binding;
+
+    // Ranges to search in BPT. They are not local variables because some positions are reused.
+    std::array<uint64_t, 4> min_ids;
+    std::array<uint64_t, 4> max_ids;
+
+    // Structs for BFS
+    std::set<SearchState> visited;
+    std::queue<SearchState> open;
+
+    // Statistics
+    uint_fast32_t results_found = 0;
+    uint_fast32_t bpt_searches = 0;
+
+public:
+    PropertyPathEnum(BPlusTree<4>& type_from_to_edge,
+                      BPlusTree<4>& to_type_from_edge,
+                      Id start,
+                      VarId end,
+                      PathAutomaton automaton);
+    ~PropertyPathEnum() = default;
+
+    void analyze(int indent = 0) const override;
+    void begin(BindingId& parent_binding, bool parent_has_next) override;
+    void reset() override;
+    void assign_nulls() override;
+    bool next() override;
+};
+
+#endif // RELATIONAL_MODEL__PROPERTY_PATH_ENUM_H_
