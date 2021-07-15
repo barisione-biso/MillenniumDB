cb197a7a9325609accaede0aec4ea94cad8f21f8
Carlos Rojas <cirojas6@uc.cl>
2021-07-15 14:30:31 -0400

    reorganized tests

--- tests/test_grammar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#execute from the project root
-for filename in ./tests/succeed/q*.txt; do
-  printf "Testing $(basename $filename): "
-  ./build/Release/bin/query "$filename" > /dev/null && echo "Success!"
-  # compare using diff and ignoring last line (time execution)
-  # diff <(head -n -1 result) solution # TODO: ver si < funciona o debo ajustarlo a bash
-done
--- tests/test_queries.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#execute from the project root
-for filename in ./tests/queries/*.query; do
-  printf "Testing $(basename $filename): "
-  ./build/Release/bin/query "$filename" > /dev/null && echo "Success!"
-  # TODO: compare result with diff
-done
