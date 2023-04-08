45abbaaa0c1fc6517d536b8ffc6611a5f9b17492
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2023-04-08 08:44:40 -0400

    full ramdisk no limit

--- wco_cds_paper/benchmark_server_full_ramdisk_no_limit.sh
+++ wco_cds_paper/benchmark_server_full_ramdisk_no_limit.sh
@@ -1,2 +1,6 @@
+sudo umount /mnt/tmp
+sudo mount -t tmpfs -o size=210g tmpfs /mnt/tmp
+cp -R /data/MilleniumDB-Dev /mnt/tmp
+cd /mnt/tmp/MillenniumDB-Dev
 sudo sync; sudo echo 3 > /proc/sys/vm/drop_caches
-python3 benchmark_mdb.py '/mnt/tmp/MillenniumDB-Dev' 'tests/dbs/wikipedia_db_full' 'output_queries_wikidata_full_no_limit.txt' 'benchmark_results_full_ramdisk.tsv'
+python3 benchmark_mdb.py '/mnt/tmp/MillenniumDB-Dev' 'tests/dbs/wikipedia_db_full' 'output_queries_wikidata_full_no_limit.txt' 'benchmark_results_full_ramdisk_no_limit.tsv'
